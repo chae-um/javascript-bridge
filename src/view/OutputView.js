06ed632dfef796ef916b15e8a156af6f583a47aa
ryan-dia <cjfdnjs158@naver.com>
2023-12-03 07:58:32 +0900

    feat: 게임 시작 메시지 출력

--- src/view/OutputView.js
+++ src/view/OutputView.js
@@ -1,7 +1,24 @@
+import { Console } from '@woowacourse/mission-utils';
+
+const INPUT_MESSAGE = {
+  start: '다리 건너기 게임을 시작합니다.\n',
+};
+
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 const OutputView = {
+  /**
+   * @param {string} message
+   */
+  print(message) {
+    Console.print(message);
+  },
+
+  printStart() {
+    this.print(INPUT_MESSAGE.start);
+  },
+
   /**
    * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
    * <p>
