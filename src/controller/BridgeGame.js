566c64cbf566ab1f29f7d65dd3218f18cd895a5d
ryan-dia <cjfdnjs158@naver.com>
2023-12-03 09:28:30 +0900

    feat: 플레이어가 이동할 칸 입력 받기

--- src/controller/BridgeGame.js
+++ src/controller/BridgeGame.js
@@ -18,6 +18,8 @@ class BridgeGame {
   async startGame() {
     this.#outputView.printStart();
     const bridge = await this.getBridge();
+
+    await this.#move(bridge);
   }
 
   async getBridge() {
@@ -38,7 +40,15 @@ class BridgeGame {
    * <p>
    * 이동을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  move() {}
+  async #move() {
+    const moving = await this.#handleError(() => this.#readMoving());
+  }
+
+  async #readMoving() {
+    const moving = await this.#inputView.readMoving();
+
+    return moving;
+  }
 
   /**
    * 사용자가 게임을 다시 시도할 때 사용하는 메서드
--- src/view/InputView.js
+++ src/view/InputView.js
@@ -3,6 +3,7 @@ import { validateEmptyString } from '../utils/validators/index.js';
 
 const INPUT_MESSAGE = {
   bridgeSize: '다리의 길이를 입력해주세요.\n',
+  moving: '\n이동할 칸을 선택해주세요. (위: U, 아래: D)\n',
 };
 
 /**
@@ -26,8 +27,15 @@ const InputView = {
 
   /**
    * 사용자가 이동할 칸을 입력받는다.
+   * @returns {Promise<string>}
    */
-  readMoving() {},
+  async readMoving() {
+    const moving = await Console.readLineAsync(INPUT_MESSAGE.moving);
+
+    validateEmptyString(moving);
+
+    return moving;
+  },
 
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
